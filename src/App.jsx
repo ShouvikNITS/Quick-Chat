00e3fe5e3ed3adef8f02627aa5e30bb6340248d0
Shouvik Paul <153314600+ShouvikNITS@users.noreply.github.com>
2025-07-10 15:58:31 +0530

    Add routing and main components for chat application
    - Implemented routing in App component with Homepage, Loginpage, and Profilepage.
    - Created Sidebar, ChatContainer, and RightSidebar components for the chat interface.
    - Added Loginpage with form handling for user authentication.
    - Added utility function for formatting message timestamps.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Homepage from './pages/Homepage.jsx'
+import Loginpage from './pages/loginpage.jsx'
+import Profilepage from './pages/profilepage.jsx'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className=" bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/login' element={<Loginpage />} />
+        <Route path='/profile' element={<Profilepage />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import Sidebar from '../components/Sidebar'
+import ChatContainer from '../components/ChatContainer'
+import RightSidebar from '../components/RightSidebar'
+
+const Homepage = () => {
+
+  const [selectedUser, setSelectedUser] = useState(false)
+
+
+  return (
+    <div className=' border w-full h-screen sm:px-[15%] sm:py-[5%]'>
+    <div className={ `backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative 
+                      ${selectedUser ? ' md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : ' grid-cols-2'}`}>
+      <Sidebar selectedUser={selectedUser} setSelectUser={setSelectedUser} />
+      <ChatContainer selectedUser={selectedUser} setSelectUser={setSelectedUser} />
+      <RightSidebar selectedUser={selectedUser} setSelectUser={setSelectedUser} />
+    </div>
+    </div>
+  )
+}
+
+export default Homepage
