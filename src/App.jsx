520b8bbbc15f7b9b2b278219be3ff67e9fec890a
Shouvik Paul <153314600+ShouvikNITS@users.noreply.github.com>
2025-07-11 01:04:55 +0530

    Implement authentication-based routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Homepage from './pages/Homepage.jsx'
 import Loginpage from './pages/loginpage.jsx'
 import Profilepage from './pages/profilepage.jsx'
 import { Toaster } from 'react-hot-toast'
+import { authcontext } from '../context/authContext.jsx'
 
 const App = () => {
+
+  const {authUser} = useContext(authcontext)
+
   return (
     <div className=" bg-[url('./src/assets/bgImage.svg')] bg-contain">
     <Toaster />
       <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/login' element={<Loginpage />} />
-        <Route path='/profile' element={<Profilepage />} />
+        <Route path='/' element={authUser? <Homepage /> : <Navigate to='/login' />} />
+        <Route path='/login' element={!authUser ? <Loginpage /> : <Navigate to='/' />} />
+        <Route path='/profile' element={authUser ? <Profilepage /> : <Navigate to='/login' />} />
       </Routes>
     </div>
   )
